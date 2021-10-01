bcc41c77985a0f4657f6f79c6cd5a682aedc8500
amy-chinlai <amy@actionnetwork.org>
2021-10-01 12:55:38 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+    use Rack::Flash
+    enable :sessions
 
     get '/songs' do
         @songs = Song.all
@@ -11,15 +15,28 @@ class SongsController < ApplicationController
     end
 
     post '/songs' do
-        # binding.pry
         @song = Song.create(name: params[:Name])
         @song.artist = Artist.find_or_create_by(name: params[:artist_name])
-        @genre = Genre.find_by_id(id: params[:genres][0].to_i)
+        @genre = Genre.find_by_id(params[:genres][0].to_i)
         @song.song_genres.create(genre: @genre)
-        binding.pry
         @song.save
-        # @song.song_genres.save
+        flash[:message] = "Successfully created song."
+        redirect to "/songs/#{@song.slug}"
+    end
+
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        @genres = Genre.all
         # binding.pry
+        erb :'songs/edit'
+    end
+
+    patch '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        @song.update(params)
+        @song.genre_ids = params[:genres]
+        binding.pry
+        flash[:message] = "Successfully updated song."
         redirect to "/songs/#{@song.slug}"
     end
 
